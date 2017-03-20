a54590a2be33346adf2ff8173f5cc053682957b7
Anatoliy Klots and Starlynne Start <anatoliy@>
2017-03-20 16:07:54 -0700

    add jQuery functions, css styles, img

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("h1").click(function() {
+    alert("This is a header");
+  });
+  $("p").click(function() {
+    alert("This is a paragraph!");
+  });
+  $("div").click(function() {
+    alert("This is a div");
+  });
+  $("img").click(function() {
+    alert("This is a cat image!")
+  });
+  $("span").click(function() {
+    alert("And this is a span")
+  });
+
+});
